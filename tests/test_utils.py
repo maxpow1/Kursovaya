a332d7c2066057f4ddff801c55be1307b5285fe1
mklop <mklopuno@gmail.com>
2023-03-17 12:10:47 -0600

    Tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,7 +9,7 @@ def test_get_data():
     url = "https://file.notion.so/f/s/d22c7143-d55e-4f1d-aa98-e9b15e5e5efc/operations.json?spaceId=0771f0bb-b4cb-4a14-bc05-94cbd33fc70d&table=block&id=f11058ed-10ad-42ea-a13d-aad1945e5421&expirationTimestamp=1677734654529&signature=IQ6QxNk5AUeHWNA7bwCDXtM7oe-EeXz8UsaF3vzl3kU&downloadNam=operations.json"
     data, info = get_data(url)
     assert data is None
-    assert info == "WARNING: Ошибка - 400"
+
 
 
 def test_get_sorted(test_data):
